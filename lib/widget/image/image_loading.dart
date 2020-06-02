1ee075aa1e47c3d7afef260e09259eed7c03c450
zqhua <zqhua@zqhuadeMac-mini.local>
2020-06-02 11:10:36 +0800

    image_loading  网络图片加载动画

---
+++ lib/widget/image/image_loading.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+
+class ImageByNetWork extends StatefulWidget {
+  final String src;
+  final double scale;
+  final Widget loading;
+  final Widget errorLoading;
+
+  /// If non-null, require the image to have this width.
+  ///
+  /// If null, the image will pick a size that best preserves its intrinsic
+  /// aspect ratio.
+  ///
+  /// It is strongly recommended that either both the [width] and the [height]
+  /// be specified, or that the widget be placed in a context that sets tight
+  /// layout constraints, so that the image does not change size as it loads.
+  /// Consider using [fit] to adapt the image's rendering to fit the given width
+  /// and height if the exact image dimensions are not known in advance.
+  final double width;
+
+  /// If non-null, require the image to have this height.
+  ///
+  /// If null, the image will pick a size that best preserves its intrinsic
+  /// aspect ratio.
+  ///
+  /// It is strongly recommended that either both the [width] and the [height]
+  /// be specified, or that the widget be placed in a context that sets tight
+  /// layout constraints, so that the image does not change size as it loads.
+  /// Consider using [fit] to adapt the image's rendering to fit the given width
+  /// and height if the exact image dimensions are not known in advance.
+  final double height;
+
+  /// If non-null, this color is blended with each image pixel using [colorBlendMode].
+  final Color color;
+
+  /// Used to set the [FilterQuality] of the image.
+  ///
+  /// Use the [FilterQuality.low] quality setting to scale the image with
+  /// bilinear interpolation, or the [FilterQuality.none] which corresponds
+  /// to nearest-neighbor.
+  final FilterQuality filterQuality;
+
+  /// Used to combine [color] with this image.
+  ///
+  /// The default is [BlendMode.srcIn]. In terms of the blend mode, [color] is
+  /// the source and this image is the destination.
+  ///
+  /// See also:
+  ///
+  ///  * [BlendMode], which includes an illustration of the effect of each blend mode.
+  final BlendMode colorBlendMode;
+
+  /// How to inscribe the image into the space allocated during layout.
+  ///
+  /// The default varies based on the other fields. See the discussion at
+  /// [paintImage].
+  final BoxFit fit;
+
+  /// How to align the image within its bounds.
+  ///
+  /// The alignment aligns the given position in the image to the given position
+  /// in the layout bounds. For example, an [Alignment] alignment of (-1.0,
+  /// -1.0) aligns the image to the top-left corner of its layout bounds, while an
+  /// [Alignment] alignment of (1.0, 1.0) aligns the bottom right of the
+  /// image with the bottom right corner of its layout bounds. Similarly, an
+  /// alignment of (0.0, 1.0) aligns the bottom middle of the image with the
+  /// middle of the bottom edge of its layout bounds.
+  ///
+  /// To display a subpart of an image, consider using a [CustomPainter] and
+  /// [Canvas.drawImageRect].
+  ///
+  /// If the [alignment] is [TextDirection]-dependent (i.e. if it is a
+  /// [AlignmentDirectional]), then an ambient [Directionality] widget
+  /// must be in scope.
+  ///
+  /// Defaults to [Alignment.center].
+  ///
+  /// See also:
+  ///
+  ///  * [Alignment], a class with convenient constants typically used to
+  ///    specify an [AlignmentGeometry].
+  ///  * [AlignmentDirectional], like [Alignment] for specifying alignments
+  ///    relative to text direction.
+  final AlignmentGeometry alignment;
+
+  /// How to paint any portions of the layout bounds not covered by the image.
+  final ImageRepeat repeat;
+
+  /// The center slice for a nine-patch image.
+  ///
+  /// The region of the image inside the center slice will be stretched both
+  /// horizontally and vertically to fit the image into its destination. The
+  /// region of the image above and below the center slice will be stretched
+  /// only horizontally and the region of the image to the left and right of
+  /// the center slice will be stretched only vertically.
+  final Rect centerSlice;
+
+  /// Whether to paint the image in the direction of the [TextDirection].
+  ///
+  /// If this is true, then in [TextDirection.ltr] contexts, the image will be
+  /// drawn with its origin in the top left (the "normal" painting direction for
+  /// images); and in [TextDirection.rtl] contexts, the image will be drawn with
+  /// a scaling factor of -1 in the horizontal direction so that the origin is
+  /// in the top right.
+  ///
+  /// This is occasionally used with images in right-to-left environments, for
+  /// images that were designed for left-to-right locales. Be careful, when
+  /// using this, to not flip images with integral shadows, text, or other
+  /// effects that will look incorrect when flipped.
+  ///
+  /// If this is true, there must be an ambient [Directionality] widget in
+  /// scope.
+  final bool matchTextDirection;
+
+  /// Whether to continue showing the old image (true), or briefly show nothing
+  /// (false), when the image provider changes.
+  final bool gaplessPlayback;
+
+  /// Whether to paint the image with anti-aliasing.
+  ///
+  /// Anti-aliasing alleviates the sawtooth artifact when the image is rotated.
+  final bool isAntiAlias;
+  ImageByNetWork(
+    this.src, {
+    Key key,
+    this.scale = 1.0,
+    this.width,
+    this.height,
+    this.color,
+    this.colorBlendMode,
+    this.fit,
+    this.alignment = Alignment.center,
+    this.repeat = ImageRepeat.noRepeat,
+    this.centerSlice,
+    this.matchTextDirection = false,
+    this.gaplessPlayback = false,
+    this.filterQuality = FilterQuality.low,
+    this.isAntiAlias = false,
+    this.loading,
+    this.errorLoading,
+    Map<String, String> headers,
+    int cacheWidth,
+    int cacheHeight,
+  })  : assert(alignment != null),
+        assert(repeat != null),
+        assert(matchTextDirection != null),
+        assert(cacheWidth == null || cacheWidth > 0),
+        assert(cacheHeight == null || cacheHeight > 0),
+        assert(isAntiAlias != null),
+        super(key: key);
+  @override
+  _ImageByNetWorkState createState() => _ImageByNetWorkState();
+}
+
+class _ImageByNetWorkState extends State<ImageByNetWork> {
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      widget.src,
+      scale: widget.scale,
+      width: widget.width,
+      height: widget.height,
+      color: widget.color ?? Colors.transparent,
+      colorBlendMode: widget.colorBlendMode,
+      fit: widget.fit,
+      alignment: widget.alignment,
+      repeat: widget.repeat,
+      centerSlice: widget.centerSlice,
+      matchTextDirection: widget.matchTextDirection,
+      gaplessPlayback: widget.gaplessPlayback,
+      filterQuality: widget.filterQuality,
+      isAntiAlias: widget.isAntiAlias,
+
+      /// 展示frame
+      frameBuilder: (ctx, v, int frame, bool wasSynchronouslyLoaded) {
+        if (wasSynchronouslyLoaded) {
+          return v;
+        }
+        return AnimatedOpacity(
+          child: v,
+          opacity: frame == null ? 0 : 1,
+          duration: const Duration(seconds: 1),
+          curve: Curves.easeOut,
+        );
+      },
+
+      /// 加载动画
+      loadingBuilder: (ctx, v, event) {
+        if (event == null) return v;
+        return widget.loading ??
+            Center(
+              child: CircularProgressIndicator(
+                backgroundColor: widget.color ?? Theme.of(context).accentColor,
+                value: event.expectedTotalBytes != null
+                    ? event.cumulativeBytesLoaded / event.expectedTotalBytes
+                    : null,
+              ),
+            );
+      },
+
+      /// 加载失败
+      errorBuilder: (ctx, obj, stackTrace) {
+        return widget.errorLoading ??
+            Center(
+              child: Text(
+                '😢',
+                style: TextStyle(color: Colors.red),
+              ),
+            );
+      },
+    );
+  }
+}
